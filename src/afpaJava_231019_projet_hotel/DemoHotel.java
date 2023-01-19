582b2db86313354c6cd64dd41c0a42d28c955a94
yzapart <87938906+yzapart@users.noreply.github.com>
2023-01-19 12:12:57 +0100

    Add files via upload

--- src/afpaJava_231019_projet_hotel/DemoHotel.java
+++ src/afpaJava_231019_projet_hotel/DemoHotel.java
@@ -1,57 +1,61 @@
 package afpaJava_231019_projet_hotel;
+
 import java.lang.Math;
+import java.util.ArrayList;
 
 public class DemoHotel {
-	static String listeType[] = {"single", "twin", "double", "suite"};
+	static String listeType[] = { "single", "twin", "double", "suite" };
 	static int N_CHAMBRES = 20;
 
 	public static void main(String[] args) {
-		
+
 		creationChambres(N_CHAMBRES);
 		Menu.afficherMenu("");
-		
+
 		
 	}
-	
-	
-	
-	
+
 	public static void creationChambres(int n) {
 		// Création aléatoire de n chambres.
-				for (int i=0; i<n; i++) {
-					Boolean etat = (int)Math.floor(Math.random()*2) == 0 ? false : true;
-					String type = listeType[(int)Math.floor(Math.random()*4)];
-					new Chambre(Chambre.listeDesChambres.size(), type, etat);
-				}
+		for (int i = 0; i < n; i++) {
+			Boolean etat = (int) Math.floor(Math.random() * 2) == 0 ? false : true;
+			String type = listeType[(int) Math.floor(Math.random() * 4)];
+			new Chambre(Chambre.listeDesChambres.size(), type, etat);
+		}
 	}
-	
+
 	public static void tests() {
 		Hotel.afficherEtatHotel();
-		
+
 		// Affichage première chambre vide de chaque type
-		// Est-ce qu'il vaut mieux inclure le test si une chambre est disponible dans la méthode numPremoièreChambreVide de la classe Chambre ?
+		// Est-ce qu'il vaut mieux inclure le test si une chambre est disponible dans la
+		// méthode numPremoièreChambreVide de la classe Chambre ?
 		// Dans ce cas celle-ci retournerait une valeur String.
-		for (String type: listeType) {
-			String res = Chambre.numPremiereChambreVide(type) == 999 ? "Aucune chambre libre" : Integer.toString(Chambre.numPremiereChambreVide(type));
+		for (String type : listeType) {
+			String res = Chambre.numPremiereChambreVide(type) == 999 ? "Aucune chambre libre"
+					: Integer.toString(Chambre.numPremiereChambreVide(type));
 			System.out.println("N° première chambre vide (type " + type + ")\t: " + res);
 		}
 		System.out.println(("---"));
 
 		// Affichage dernière chambre vide de chaque type
-		for (String type: listeType) {
-			String res = Chambre.numDerniereChambreVide(type) == 999 ? "Aucune chambre libre" : Integer.toString(Chambre.numDerniereChambreVide(type));
+		for (String type : listeType) {
+			String res = Chambre.numDerniereChambreVide(type) == 999 ? "Aucune chambre libre"
+					: Integer.toString(Chambre.numDerniereChambreVide(type));
 			System.out.println("N° dernière chambre vide (type " + type + ")\t: " + res);
 		}
 		System.out.println(("---"));
-		
+
 		// Réserver première chambre vide du type "single"
-		System.out.println("Réserver première chambre vide type single (n° " + Chambre.numPremiereChambreVide("single") + ") :");
+		System.out.println(
+				"Réserver première chambre vide type single (n° " + Chambre.numPremiereChambreVide("single") + ") :");
 		Chambre.reserverChambre(Chambre.numPremiereChambreVide("single"));
 		Hotel.afficherEtatHotel();
 		System.out.println(("---"));
-		
+
 		// Libérer dernière chambre occupée du type "single"
-		System.out.println("Libérer la dernière chambre vide type single (n° " + Chambre.numDerniereChambreOccupee("single") + ") :");
+		System.out.println("Libérer la dernière chambre vide type single (n° "
+				+ Chambre.numDerniereChambreOccupee("single") + ") :");
 		Chambre.libererChambre(Chambre.numDerniereChambreOccupee("single"));
 		Hotel.afficherEtatHotel();
 		System.out.println(("---"));
--- src/afpaJava_231019_projet_hotel/Menu.java
+++ src/afpaJava_231019_projet_hotel/Menu.java
@@ -10,6 +10,7 @@ public class Menu {
 			"E: Afficher le numéro de la dernière chambre occupée", " ", "F: Réserver une chambre",
 			"G: Libérer une chambre", " ", "Q: Quitter" };
 
+	
 	static Scanner scan = new Scanner(System.in);
 
 	static void afficherMenu(String message) {
@@ -36,9 +37,9 @@ public class Menu {
 		case "e":
 			menu5();
 			break;
-//		case "f": 
-//			menu6();
-//			break;
+		case "f": 
+			menu6();
+			break;
 //		case "g": 
 //			menu7();
 //			break;
@@ -130,8 +131,146 @@ public class Menu {
 		System.out.println("N° dernière chambre occupée (choixType " + choixType + ")\t: " + res);
 		retour();
 	}
+	
+	// réserver première chambre vide avec authentification
+	// ======================== il faut gérer le cas où il n'y a pas de chambre vide ==============================
+	static void menu6() {
+		cls();
+		System.out.println("Entrer identifiant : ");
+		String user = scan.nextLine().toLowerCase();
+		System.out.println("Mot de passe : ");
+		String mdp = scan.nextLine().toLowerCase();
+		if (loginOk(user, mdp) == true) {
+			// réservation chambre
+			selectionType();
+			String choixType = typeSelectionne();
+			System.out.println("Chambre n°" + Chambre.numPremiereChambreVide(choixType) + " réservée.");
+			Chambre.reserverChambre(Chambre.numPremiereChambreVide(choixType));
+		} else {
+			System.out.println("Identification échouée.");
+		}
+		retour();
+	}
+	
+	
+	
+	
+	
+	
+	
+	static boolean loginOk(String user, String mdp) {
+		ArrayList<String[]> listeLogin = new ArrayList<String[]>();
 
+		String logins[][] = { 
+								{ "user", "mdp" }, 
+								{ "toto", "tata" }, 
+								{ "allo", "bobo" } 
+							};
 
+		for (String[] l : logins) {
+			listeLogin.add(l);
+		}
+
+		for (String[] l : listeLogin) {
+			if (l[0].equals(user) && l[1].equals(mdp)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+
+	//
+//	// créer habitation
+//	static void menu2() {
+//		cls();
+//		System.out.println("Propriétaire :");
+//		scan.nextLine(); String proprietaire = scan.nextLine();
+//		System.out.println("Adresse :");
+//		String adresse = scan.nextLine();
+//		System.out.println("Surface :");
+//		double surface = scan.nextDouble();
+//		System.out.println("Type (1: individuelle / 2: professionnelle");
+//		int typeChoix = scan.nextInt();
+//		if (typeChoix == 1) {
+//			String type = "individuelle";
+//			System.out.println("Nombre de pièces :");
+//			int nbPieces = scan.nextInt();
+//			System.out.println("Présence piscine (1: oui / 2: non");
+//			boolean piscine = scan.nextInt() == 1 ? true : false;
+//			new HabitationIndividuelle(proprietaire, adresse, surface, nbPieces, piscine);
+//		} else {
+//			String type = "professionnelle";
+//			System.out.println("Nombre d'employés :");
+//			int nbEmployes= scan.nextInt();
+//			new HabitationProfessionnelle(proprietaire, adresse, surface, nbEmployes);
+//		}
+//		afficherMenu("");
+//	}
+//	
+//
+//	// ajouter habitation à une commune
+//	static void menu3() {
+//		cls();
+//		Habitation.afficherListeDeToutesLesHabitations();
+//		System.out.println("Sélection n° habitation :");
+//		int choixHab = scan.nextInt();
+//		
+//		Commune.afficherListeDesCommunes();
+//		System.out.println("Sélection n° commune :");
+//		int choixCommune = scan.nextInt();
+//		
+//		Commune.listeDesCommunes.get(choixCommune - 1).ajoutHabitation(Habitation.listeDeToutesLesHabitations.get(choixHab - 1));
+//		afficherMenu("L'habitation n°" + choixHab + " a été ajoutée à la commune de " + Commune.listeDesCommunes.get(choixCommune - 1).getNom());;
+//	}
+//	
+//	// modifier proprio
+//	static void menu4() {
+//		cls();
+//		Habitation.afficherListeDeToutesLesHabitations();
+//		System.out.println("Sélection n° habitation :");
+//		int choixHab = scan.nextInt();
+//		Habitation habitationChoisie = Habitation.listeDeToutesLesHabitations.get(choixHab - 1);
+//		
+//		System.out.println("Nom du propriétaire :");
+//		scan.nextLine(); String nouveauProprio = scan.nextLine();
+//		
+//		habitationChoisie.setProprietaire(nouveauProprio);
+//		afficherMenu("Modification propriétaire habitation n°" + choixHab + "\tnom :" + nouveauProprio);
+//		
+//	}
+//	
+//	static void menu5() {
+//		cls();
+//		Habitation.afficherListeDeToutesLesHabitations();
+//		System.out.println("(Retour : Enter)");
+//		String s = scan.nextLine();s = scan.nextLine();
+//		afficherMenu("");
+//	}
+//
+//	static void menu6() {
+//		cls();
+//		Commune.afficherListeDesCommunes();
+//		System.out.println("Sélection n° commune :");
+//		int choixCommune = scan.nextInt();
+//		Commune communeChoisie = Commune.listeDesCommunes.get(choixCommune - 1);
+//		communeChoisie.afficherHabitations();
+//		
+//		System.out.println("(Retour : Enter)");
+//		Scanner s = new Scanner(System.in);
+//		s.nextLine();
+//		afficherMenu("");
+//	}
+//
+//	static void menu7() {
+//		cls();
+//		Commune.afficherListeDesCommunes();
+//		System.out.println("(Retour : Enter)");
+//		Scanner s = new Scanner(System.in);
+//		s.nextLine();
+//		afficherMenu("");
+//		
+//	}
 
 	static void menu8() {
 		cls();
