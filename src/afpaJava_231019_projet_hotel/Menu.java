70ea3012e473c2f80b6ca07b8aaa58336d41919e
yzapart <87938906+yzapart@users.noreply.github.com>
2023-01-19 12:18:55 +0100

    Add files via upload

--- src/afpaJava_231019_projet_hotel/Menu.java
+++ src/afpaJava_231019_projet_hotel/Menu.java
@@ -5,10 +5,19 @@ import java.util.ArrayList;
 
 public class Menu {
 
-	static String listeMenu[] = { "A: Afficher état de l'hôtel", " ", "B: Afficher le nombre de chambres réservées",
-			"C: Afficher le nombre de chambres libres", " ", "D: Afficher le numéro de la première chambre vide",
-			"E: Afficher le numéro de la dernière chambre occupée", " ", "F: Réserver une chambre",
-			"G: Libérer une chambre", " ", "Q: Quitter" };
+	static String listeMenu[] = { "A: Afficher état de l'hôtel", 
+								  " ", 
+								  "B: Afficher le nombre de chambres réservées",
+								  "C: Afficher le nombre de chambres libres", 
+								  " ", 
+								  "D: Afficher le numéro de la première chambre vide",
+								  "E: Afficher le numéro de la dernière chambre occupée", 
+								  " ", 
+								  "F: Réserver une chambre",
+								  "G: Libérer une chambre", 
+								  " ", 
+								  "Q: Quitter" 
+								};
 
 	
 	static Scanner scan = new Scanner(System.in);
@@ -40,9 +49,9 @@ public class Menu {
 		case "f": 
 			menu6();
 			break;
-//		case "g": 
-//			menu7();
-//			break;
+		case "g": 
+			menu7();
+			break;
 		case "q":
 			menu8();
 			break;
@@ -90,6 +99,28 @@ public class Menu {
 		return choixType;
 	}
 	
+	static boolean loginOk(String user, String mdp) {
+		ArrayList<String[]> listeLogin = new ArrayList<String[]>();
+
+		String logins[][] = { 
+								{ "user", "mdp" }, 
+								{ "toto", "tata" }, 
+								{ "allo", "bobo" } 
+							};
+
+		for (String[] l : logins) {
+			listeLogin.add(l);
+		}
+
+		for (String[] l : listeLogin) {
+			if (l[0].equals(user) && l[1].equals(mdp)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
 
 	// afficher état hotel
 	static void menu1() {
@@ -132,6 +163,7 @@ public class Menu {
 		retour();
 	}
 	
+	
 	// réserver première chambre vide avec authentification
 	// ======================== il faut gérer le cas où il n'y a pas de chambre vide ==============================
 	static void menu6() {
@@ -152,125 +184,26 @@ public class Menu {
 		retour();
 	}
 	
-	
-	
-	
-	
-	
-	
-	static boolean loginOk(String user, String mdp) {
-		ArrayList<String[]> listeLogin = new ArrayList<String[]>();
-
-		String logins[][] = { 
-								{ "user", "mdp" }, 
-								{ "toto", "tata" }, 
-								{ "allo", "bobo" } 
-							};
-
-		for (String[] l : logins) {
-			listeLogin.add(l);
+	// libérer denrière chambre occupée avec authentification
+	// ======================== il faut gérer le cas où il n'y a pas de chambre occupée ==============================
+	static void menu7() {
+		cls();
+		System.out.println("Entrer identifiant : ");
+		String user = scan.nextLine().toLowerCase();
+		System.out.println("Mot de passe : ");
+		String mdp = scan.nextLine().toLowerCase();
+		if (loginOk(user, mdp) == true) {
+			// réservation chambre
+			selectionType();
+			String choixType = typeSelectionne();
+			System.out.println("Chambre n°" + Chambre.numDerniereChambreOccupee(choixType) + " libérée.");
+			Chambre.libererChambre(Chambre.numDerniereChambreOccupee(choixType));
+		} else {
+			System.out.println("Identification échouée.");
 		}
-
-		for (String[] l : listeLogin) {
-			if (l[0].equals(user) && l[1].equals(mdp)) {
-				return true;
-			}
-		}
-		
-		return false;
+		retour();
 	}
-
-	//
-//	// créer habitation
-//	static void menu2() {
-//		cls();
-//		System.out.println("Propriétaire :");
-//		scan.nextLine(); String proprietaire = scan.nextLine();
-//		System.out.println("Adresse :");
-//		String adresse = scan.nextLine();
-//		System.out.println("Surface :");
-//		double surface = scan.nextDouble();
-//		System.out.println("Type (1: individuelle / 2: professionnelle");
-//		int typeChoix = scan.nextInt();
-//		if (typeChoix == 1) {
-//			String type = "individuelle";
-//			System.out.println("Nombre de pièces :");
-//			int nbPieces = scan.nextInt();
-//			System.out.println("Présence piscine (1: oui / 2: non");
-//			boolean piscine = scan.nextInt() == 1 ? true : false;
-//			new HabitationIndividuelle(proprietaire, adresse, surface, nbPieces, piscine);
-//		} else {
-//			String type = "professionnelle";
-//			System.out.println("Nombre d'employés :");
-//			int nbEmployes= scan.nextInt();
-//			new HabitationProfessionnelle(proprietaire, adresse, surface, nbEmployes);
-//		}
-//		afficherMenu("");
-//	}
-//	
-//
-//	// ajouter habitation à une commune
-//	static void menu3() {
-//		cls();
-//		Habitation.afficherListeDeToutesLesHabitations();
-//		System.out.println("Sélection n° habitation :");
-//		int choixHab = scan.nextInt();
-//		
-//		Commune.afficherListeDesCommunes();
-//		System.out.println("Sélection n° commune :");
-//		int choixCommune = scan.nextInt();
-//		
-//		Commune.listeDesCommunes.get(choixCommune - 1).ajoutHabitation(Habitation.listeDeToutesLesHabitations.get(choixHab - 1));
-//		afficherMenu("L'habitation n°" + choixHab + " a été ajoutée à la commune de " + Commune.listeDesCommunes.get(choixCommune - 1).getNom());;
-//	}
-//	
-//	// modifier proprio
-//	static void menu4() {
-//		cls();
-//		Habitation.afficherListeDeToutesLesHabitations();
-//		System.out.println("Sélection n° habitation :");
-//		int choixHab = scan.nextInt();
-//		Habitation habitationChoisie = Habitation.listeDeToutesLesHabitations.get(choixHab - 1);
-//		
-//		System.out.println("Nom du propriétaire :");
-//		scan.nextLine(); String nouveauProprio = scan.nextLine();
-//		
-//		habitationChoisie.setProprietaire(nouveauProprio);
-//		afficherMenu("Modification propriétaire habitation n°" + choixHab + "\tnom :" + nouveauProprio);
-//		
-//	}
-//	
-//	static void menu5() {
-//		cls();
-//		Habitation.afficherListeDeToutesLesHabitations();
-//		System.out.println("(Retour : Enter)");
-//		String s = scan.nextLine();s = scan.nextLine();
-//		afficherMenu("");
-//	}
-//
-//	static void menu6() {
-//		cls();
-//		Commune.afficherListeDesCommunes();
-//		System.out.println("Sélection n° commune :");
-//		int choixCommune = scan.nextInt();
-//		Commune communeChoisie = Commune.listeDesCommunes.get(choixCommune - 1);
-//		communeChoisie.afficherHabitations();
-//		
-//		System.out.println("(Retour : Enter)");
-//		Scanner s = new Scanner(System.in);
-//		s.nextLine();
-//		afficherMenu("");
-//	}
-//
-//	static void menu7() {
-//		cls();
-//		Commune.afficherListeDesCommunes();
-//		System.out.println("(Retour : Enter)");
-//		Scanner s = new Scanner(System.in);
-//		s.nextLine();
-//		afficherMenu("");
-//		
-//	}
+	
 
 	static void menu8() {
 		cls();
